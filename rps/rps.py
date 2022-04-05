5f5878d0c4fd0ec67cc7c7c85d3f2dba73570e2a
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-04-05 16:13:38 +0100

    finished rps game in python

---
+++ rps/rps.py
@@ -0,0 +1,23 @@
+import random
+
+def play():
+  user = input("type r for rock, p for paper and s for scissors: ")
+  computer = random.choice(['r', 'p', 's'])
+
+  if user == computer:
+    print("It's a tie!")
+    return
+  
+  if youWin(user, computer):
+    print("You win!")
+    return
+  
+  print("You lose!")
+
+  
+
+def youWin(user, computer):
+  if (user == 's' and computer == 'p') or (user == 'p' and computer == 's') or (user == 'r' and computer == 's'):
+    return True
+
+play();
